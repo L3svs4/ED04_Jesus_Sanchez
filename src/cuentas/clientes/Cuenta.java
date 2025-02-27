0931c445ee4dd7546635fbdffcc1e1420a090523
Jesus Sanchez (L3SVS) <166231895+L3svs4@users.noreply.github.com>
2025-02-27 18:18:36 +0100

    Documentacion clase Cuenta
    Se ha documentado la clase Cuenta

--- src/cuentas/clientes/Cuenta.java
+++ src/cuentas/clientes/Cuenta.java
@@ -1,5 +1,12 @@
 package cuentas.clientes;
 
+/**
+ * Clase de los objetos cuenta bancaria.
+ * 
+ * @author Jesus Sanchez
+ * @version 1.0.1
+ * @since 27/02/25
+ */
 public class Cuenta {
 
 
@@ -8,6 +15,11 @@ public class Cuenta {
     private double saldo;
     private double tipoInteres;
 
+    /**
+     * Constructor por defecto.
+     * 
+     * Inicializa una cuenta vacía con saldo e interés cero.
+     */
     public Cuenta()
     {
         nombre ="";
@@ -16,6 +28,14 @@ public class Cuenta {
         tipoInteres=0;
     }
 
+    /**
+     * Constructor con parámetros.
+     * 
+     * @param nom Nombre del titular de la cuenta.
+     * @param cue Número de cuenta.
+     * @param sal Saldo inicial.
+     * @param tipo Tipo de interés de la cuenta.
+     */
     public Cuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -24,18 +44,35 @@ public class Cuenta {
         tipoInteres=tipo;
     }
 
+    /**
+     * Consulta el saldo actual de la cuenta.
+     * 
+     * @return Saldo actual de la cuenta.
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Ingresa una cantidad en la cuenta.
+     * 
+     * @param cantidad Cantidad a ingresar.
+     * @throws Captura excepción si la cantidad es negativa.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+    /**
+     * Retira una cantidad de la cuenta.
+     * 
+     * @param cantidad Cantidad a retirar.
+     * @throws Captura excepción si la cantidad es negativa o superior al saldo disponible.
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -46,56 +83,72 @@ public class Cuenta {
     }
 
     /**
-     * @return the nombre
+     * Devuelve el nombre del titular.
+     * 
+     * @return Nombre del titular.
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Establece el nombre del titular de la cuenta.
+     * 
+     * @param nombre Nombre del titular.
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Obtiene el número de cuenta.
+     * 
+     * @return Número de cuenta.
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Establece el número de cuenta.
+     * 
+     * @param cuenta Número de cuenta.
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Obtiene el saldo actual de la cuenta.
+     * 
+     * @return Saldo actual.
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Establece el saldo de la cuenta.
+     * 
+     * @param saldo Saldo a establecer.
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInteres
+     * Obtiene el tipo de interes de la cuenta.
+     * 
+     * @return Tipo de interes.
      */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
     /**
-     * @param tipoInteres the tipoInteres to set
+     * Establece el tipo de interes de la cuenta.
+     * 
+     * @param tipoInteres Tipo de interes a establecer.
      */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
