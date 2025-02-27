1540807a2db449fee9d7b9e5b12e7aacc8cfc06a
Jesus Sanchez (L3SVS) <166231895+L3svs4@users.noreply.github.com>
2025-02-27 18:09:15 +0100

    Documentacion de Main
    Se ha documentado la clase Main

--- src/cuentas/clientes/Main.java
+++ src/cuentas/clientes/Main.java
@@ -1,11 +1,32 @@
 package cuentas.clientes;
 
+/**
+ * Clase principal.
+ * 
+ * @author Jesus Sanchez
+ * @version 1.0.1
+ * @since 27/02/25
+ */
 public class Main {
 
+    /**
+     * Método principal.
+     * 
+     * @param args Argumentos de la línea de comandos (no utilizados).
+     * @throws No captura excepciones, lo hará operativa_cuenta.
+     * @return No devuelve ningún valor.
+     */
     public static void main(String[] args) {
         operativa_cuenta(0);
     }
 
+    /**
+     * Realiza operaciones sobre una cuenta bancaria: Creación, retirada e ingreso de dinero.
+     * 
+     * @param cantidad Cantidad inicial para la cuenta (No se utiliza, ha sido añadida solo para el ejercicio).
+     * @throws Captura excepciones genericas relacionadas con las operaciones de ingreso o retirada.
+     * @return No retorna ningun valor.
+     */
     private static void operativa_cuenta(float cantidad) {
         Cuenta miCuenta;
         double saldoActual;
